fbe8e0f03ea42b5327727e920229d8c0da4aea91
ivaniraldi <iraldiban@gmail.com>
2024-08-01 19:34:58 -0300

    commit inicial

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import eventsRouter from "./routes/eventsRoutes.js";
 import usersRouter from "./routes/usersRoutes.js";
 import cors from "cors";
